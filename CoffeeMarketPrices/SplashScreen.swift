cd9ec147651a9ff14c3f640dcb59d3208395a575
Rohith Thammaiah <thammaiahrohith@gmail.com>
2020-09-06 08:18:52 +0530

    :new: Renamed from "ContentView" to "SplashScreen"

--- CoffeeMarketPrices/SplashScreen.swift
+++ CoffeeMarketPrices/SplashScreen.swift
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SplashScreen: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Coffee Prices")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SplashScreen()
     }
 }
